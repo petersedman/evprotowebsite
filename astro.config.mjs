ca6e2a04144a2432bd236e083d37802d5461fc5f
Peter Sedman <psedman@yahoo.co.uk>
2026-04-14 11:56:08 +0200

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://petersedman.github.io',
+  base: '/evprotowebsite',
   vite: {
     plugins: [tailwindcss()]
   }
